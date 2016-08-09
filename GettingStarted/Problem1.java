89838a7570096790b666a45e1180eda90bf2dc50
cmis <cmis@cmis-hs07.(none)>
2016-08-09 11:46:25 +0700

    done

---
+++ GettingStarted/Problem1.java
@@ -0,0 +1,11 @@
+public class Problem1
+    {
+     public static void main ( String args[] )
+     {
+       System.out.println( "Hello World" );
+         
+        }// end method main
+       
+        
+    }// end  class __
+       
