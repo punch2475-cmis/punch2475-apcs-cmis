32fb32a9c6fce6ba3e9d740cfde98779f7f9692f
punch2475-cmis <punch2475@cmis.ac.th>
2016-08-10 21:05:34 +0700

    Done with Getting Started

--- GettingStarted/Problem4.java
+++ GettingStarted/Problem4.java
@@ -8,20 +8,28 @@ public class Problem4
        String input1 = JOptionPane.showInputDialog( "Pick first number: " );
        String input2 = JOptionPane.showInputDialog( "Pick second number: " );
  
-       int num1 = Integer.parseInt(input1);
-       int num2 = Integer.parseInt(input2);
+       float num1 = Integer.parseInt(input1);
+       float num2 = Integer.parseInt(input2);
        
-       int sum1 = num1 + num2;
-       int sum2 = num2 + num1;
-       int subtract1 = num1 - num2;
-       int subtract2 = num2 - num1;
-       int multiply1 = num1 * num2;
-       int multiply2 = num2 * num1;
-       int divide1 = num1/num2;
-       int divide2 = num2/num1;
-       int modulo1 = num1%num2;
-       int modulo2 = num2%num1;
+       float sum1 = num1 + num2;
+       float subtract1 = num1 - num2;
+       float subtract2 = num2 - num1;
+       float multiply1 = num1 * num2;
+       float divide1 = num1/num2;
+       float divide2 = num2/num1;
+       float mol1 = num1%num2;
+       float mol2 = num2%num1;
        
+       System.out.println( num1 + "+" + num2 + "=" + sum1);
+       System.out.println( num2 + "+" + num1 + "=" + sum1);
+       System.out.println( num1 + "-" + num2 + "=" + subtract1);
+       System.out.println( num2 + "-" + num1 + "=" + subtract2);
+       System.out.println( num1 + "*" + num2 + "=" + multiply1); 
+       System.out.println( num2 + "*" + num1 + "=" + multiply1); 
+       System.out.println( num1 + "/" + num2 + "=" + divide1); 
+       System.out.println( num2 + "/" + num1 + "=" + divide2);
+       System.out.println( num1 + "%" + num2 + "=" + mol1);
+       System.out.println( num2 + "%" + num1 + "=" + mol2);
 
 
         }// end method main
