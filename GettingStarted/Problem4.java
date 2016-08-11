8f5adb3c3dc27b2378e210f44ed07aa0f3e064e8
cmis <cmis@cmis-hs09.(none)>
2016-08-11 07:55:21 +0700

    DONE

--- GettingStarted/Problem4.java
+++ GettingStarted/Problem4.java
@@ -8,17 +8,17 @@ public class Problem4
        String input1 = JOptionPane.showInputDialog( "Pick first number: " );
        String input2 = JOptionPane.showInputDialog( "Pick second number: " );
  
-       float num1 = Integer.parseInt(input1);
-       float num2 = Integer.parseInt(input2);
+       int num1 = Integer.parseInt(input1);
+       int num2 = Integer.parseInt(input2);
        
-       float sum1 = num1 + num2;
-       float subtract1 = num1 - num2;
-       float subtract2 = num2 - num1;
-       float multiply1 = num1 * num2;
-       float divide1 = num1/num2;
-       float divide2 = num2/num1;
-       float mol1 = num1%num2;
-       float mol2 = num2%num1;
+       int sum1 = num1 + num2;
+       int subtract1 = num1 - num2;
+       int subtract2 = num2 - num1;
+       int multiply1 = num1 * num2;
+       int divide1 = num1/num2;
+       int divide2 = num2/num1;
+       int mol1 = num1%num2;
+       int mol2 = num2%num1;
        
        System.out.println( num1 + "+" + num2 + "=" + sum1);
        System.out.println( num2 + "+" + num1 + "=" + sum1);
