d3fe07b105e84167fc66f63a852f4bd160cdf668
punch2475-cmis <punch2475@cmis.ac.th>
2016-08-16 06:17:04 +0700

    NOT DONE

---
+++ Fundamentals/FundamentalsP0.java
@@ -0,0 +1,15 @@
+public class FundamentalsP0{
+    public static void main (string [] arg)
+    {
+        byte
+        short
+        int
+        long
+        float
+        double
+        boolean
+        char
+        
+    }//end main
+}
+   
