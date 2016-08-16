2b319f680a807c0e2adce294061ceffdfdefcfe1
cmis <cmis@cmis-hs00.(none)>
2016-08-16 11:43:42 +0700

    Done with P0 and P1

--- Fundamentals/FundamentalsP0.java
+++ Fundamentals/FundamentalsP0.java
@@ -1,15 +1,31 @@
 public class FundamentalsP0{
-    public static void main (string [] arg)
+    public static void main (String [] arg)
     {
-        byte
-        short
-        int
-        long
-        float
-        double
-        boolean
-        char
+        byte byte1= 100;
+        byte byte2= -56;
+        short short1= 10005;
+        short short2= -32541;
+        int int1= 565475;
+        int int2= -1875632;
+        long long1= -98745656;
+        long long2= 98745125;
+        float float1=2.5f;
+        float float2=256.32f;
+        double double1= 123.4;
+        double double2= 432.1;
+        boolean boolean1= true;
+        boolean boolean2= false;
+        char char1= 'P';
+        char char2= 'Q';
         
+        System.out.println(" Byte data type can be used to replace int, the maximum value of byte is 127 and the minumum value is -128. For example, " + byte1+ ", " +byte2+ ".");
+        System.out.println(" Short data type is two times smaller than int but bigger than byte, the maximum value is 32,767 and the minimum value is -32,768. For example," +short1+ ","+short2+".");
+        System.out.println(" Int data type is use for interger that maximum value is 2,147,483,647 and minium value is -2,147,483,648. For example, " + int1+ "," + int2+ ".");
+        System.out.println(" Long data type is use for a wider range than int, the maximum value is 9,223,372,036,854,775,807 and the minimum value is -9,223,372,036,854,775,807. For example, " + long1 + "," + long2 + ".");
+        System.out.println(" Float data type is use for decimal and at the end of each float you have to put f such as 23.4f. For example," + float1 + "," + float2 + ".");
+        System.out.println(" Double data type is the default data type for decimal values. For example, " + double1 + "," + double2 + ".");
+        System.out.println(" Boolean data type represent two possible valuses which is true or false. For example, " +boolean1+ "," +boolean2 + ".");
+        System.out.println(" Char data type is used to store any characters. For example, " +char1+ "," + char2 + ".");
     }//end main
 }
    
