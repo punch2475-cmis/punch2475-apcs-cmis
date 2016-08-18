87578cb9941e3976dd0db265dbf163efceeb0f12
cmis <cmis@cmis-hs00.(none)>
2016-08-18 08:40:36 +0700

    Working on P4

--- Fundamentals/FundamentalsP4.java
+++ Fundamentals/FundamentalsP4.java
@@ -1,10 +1,13 @@
+import java.util.Random;
 public class FundamentalsP4
 { 
     public static void main (String [] args)
     {
         for( int ctr = 1; ctr <= 10; ctr ++)
-        { 
-            System.out.println ( ctr +"\t" + ctr*ctr + "\t" +(int)(ctr*Math.random() ) ) ;
+        {
+            Random r= new Random();
+            int i1= r.nextInt((ctr*ctr)- ctr); 
+            System.out.println ( ctr +"\t" + ctr*ctr + "\t" +i1 );
         }//end for
     }
 }
