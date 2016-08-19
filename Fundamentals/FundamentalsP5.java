a702d0dda23a417350cd51a3bff47fe66e104eec
cmis <cmis@cmis-hs00.(none)>
2016-08-19 09:55:18 +0700

    P5

---
+++ Fundamentals/FundamentalsP5.java
@@ -0,0 +1,11 @@
+public class FundamentalsP5
+    {
+        public static void main ( String [] arg){
+            String [] favoriteMovie = { "Divergent", "Harry Potter", "Maze Runner"};
+            for( String Movies : favoriteMovie ){
+                System.out.println("My favorite movie is " + Movies);
+            
+    }
+   
+}
+}
