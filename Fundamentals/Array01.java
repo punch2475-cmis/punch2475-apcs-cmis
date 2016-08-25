810ae4edebf0da67d0b11c80ddef477f6f867e7b
cmis <cmis@cmis-hs00.(none)>
2016-08-25 09:55:20 +0700

    NOT DOne

--- Fundamentals/Array01.java
+++ Fundamentals/Array01.java
@@ -2,6 +2,12 @@ public class Array01
 {
     public static void main (String[] arg)
     {
-        int [] nums = int new {1,2,
+        int [] nums = {1,2,3,4,5,6,7,8,9,10};
+        
+       for (int index = nums.length -1; index >= 0; index--)
+       {
+           System.out.println( nums [ index]);
+        }
+        
     }
 }
---
+++ Fundamentals/Problem1.java
@@ -0,0 +1,19 @@
+public class Problem1{
+    public static void main (String [] arg)
+    {
+       int [] randomNumber = new int [1000000];
+       int x = randomNumber.length;
+       
+        
+    }
+}
+
+       /*if (x % 3 == 0 && x%5 == 0 && x%7 == 0 && x%11==0)
+       {
+           System.out.println(randomNumber);\
+        }
+       
+        else
+        {
+            System.out.println("...");
+        }/*
