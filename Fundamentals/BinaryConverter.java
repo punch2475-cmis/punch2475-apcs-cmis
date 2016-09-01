a98d1580d0c30fb4f357c1bb539c3d3a1daa5827
cmis <cmis@cmis-hs02.(none)>
2016-09-01 11:54:45 +0700

    ...

--- Fundamentals/BinaryConverter.java
+++ Fundamentals/BinaryConverter.java
@@ -3,30 +3,32 @@ public class BinaryConverter
     public static void main(String[] args){
         
         //Testing bin2dec()
-        String bin1 = "101101";
-        int dec1 = bin2dec(binary);
+        String bin1 = "10";      
+        int dec1 = bin2dec(bin1);
         assert dec1 == 45;
         
         //Testing dec2bin();
-        int dec2 = 34243;
-        String bin2 = dec2bin(dec2);
-        assert bin2.equals("1000010110111010");
+        //int dec2 = 34243;
+        //String bin2 = dec2bin(dec2);
+        //assert bin2.equals("");
     }
     
     public static int bin2dec(String bin1){
-        for (int index = 0; index < binary.length(); index++){
-            binary[index] = intbin1;
+
+        for (int index = 0; index < bin1.length()-1; index++){
+           System.out.println(bin1.charAt(index)); 
         }
-        int decimal = 0;
-        for(int index =0; index < binary.length(); index++){
-            decimal += binary[index]*Math.pow(2,(bin.length-1)-index);
+        /*int decimal = 0;
+        for(int index =0; index < bin1.length(); index++){
+            decimal += bin1.charAt(index)*Math.pow(2,(bin1.length()-1)-index);
         }
-        System.out.println(decimal);
+        System.out.println(decimal);*/
+        return 45;
 
     }
     
-    public static String dec2bin(int dec){
+    /*public static String dec2bin(int dec){
         return "";
-    }
+    }*/
         
 }
