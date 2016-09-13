933ca3ac852f0ce9fbcf0103aed52893f43c4381
Supichaya Sirimangklanurak <punch2475@cmis.ac.th>
2016-09-13 20:01:09 +0700

    hahaha TT

--- Bigproblem/Bigproblem1.java
+++ Bigproblem/Bigproblem1.java
@@ -7,16 +7,42 @@ public class Bigproblem1
         int b = Integer.parseInt(input);
         int [] randomnumbers = new int [b];
         int sum = 0;
+        int big =0; 
+        int small =0;
 
         for (int index =0; index< randomnumbers.length; index++)
         {
             randomnumbers[index] =(int) ( Math.random()*100);
             System.out.println( randomnumbers[index]);
             sum += randomnumbers[index];
+            int newran = randomnumbers[index];
+            if (index ==1)
+            {
+                small = randomnumbers[index];
+            }
+            else{
+                if (small > newran){
+                    small= randomnumbers[index];
+                }
+            }
+            if (index ==1 )
+            {
+                big = randomnumbers[index];
+            }
+            else{
+                if (big < newran)
+                {
+                    big = randomnumbers[index];
+                }
+            }
+            
         }
         int avg = sum/b;
-        System.out.print("Sum =" + sum);
-        System.out.print("Avg =" +avg);
+        System.out.println("Sum =" + sum);
+        System.out.println("Avg =" +avg);
+        System.out.println("Smallest num = " + small);
+        System.out.println("Biggest num = " + big);
+        
         
         //sum += randomnumbers 
         
@@ -30,8 +56,8 @@ public class Bigproblem1
  * //random number 0-99 
  * count how many time it occur
  * print histogram
- * print minimum 
- * print max
+ * //print minimum 
+ * //print max
  * //print sum
  * print mode
  * //print avg
