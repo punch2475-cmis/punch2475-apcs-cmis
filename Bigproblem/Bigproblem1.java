4adc3d75b0330bd2ffbba7224a9a42e6400d4a89
cmis <cmis@cmis-hs02.(none)>
2016-09-15 14:37:36 +0700

    ahhaha

--- Bigproblem/Bigproblem1.java
+++ Bigproblem/Bigproblem1.java
@@ -10,18 +10,35 @@ public class Bigproblem1
         int big =0; 
         int small =0;
         int total = 0;
-        int [] num = new int [99];
+        int [] num = new int [100];
         int index =0;
         int frequency =0;
         int count =0;
         for (int i = 0; i < num.length; i++)
         {
             num[total]++;
-            System.out.println(num[total]+":");
+            System.out.print(i + ": ");
+            int hashes = num[i];
+            while (hashes > 0)
+            {
+                System.out.print ("#");
+                hashes --;
+            }
+            
         }//it work 
+        for (int i =0; i < num.length; i++)
+        {
+            System.out.print(i + ": ");
+            int hashes = num[i];
+            while (hashes > 0)
+            {
+                System.out.print ("#");
+                hashes --;
+            }
+        }
         for (index =0; index< randomnumbers.length; index++)
         {
-             randomnumbers[index] =(int) ( Math.random()*100);
+            randomnumbers[index] =(int) ( Math.random()*100);
             System.out.println( randomnumbers[index]);
             sum += randomnumbers[index];
             int newran = randomnumbers[index];
@@ -50,7 +67,7 @@ public class Bigproblem1
         System.out.println("Avg =" +avg);
         System.out.println("Smallest num = " + small);
         System.out.println("Biggest num = " + big);
-        System.out.println("MODE =" + count);
+        System.out.println("MODE =" );
         
         //sum += randomnumbers 
         
