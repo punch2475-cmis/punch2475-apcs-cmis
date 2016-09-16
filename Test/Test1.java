f562ec5e82a66deea1f30ce00ddb329b0345f3ca
cmis <cmis@cmis-hs02.(none)>
2016-09-16 08:41:19 +0700

    ahhahah TT

---
+++ Test/Test1.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+public class Test1
+{
+    public static void main(String []arg)
+    {
+        String input = JOptionPane.showInputDialog( "Type a number: " );
+        int num = Integer.parseInt(input);
+        double [] randomnumbers = new double [num];
+        int [] randomnumbers2 = new int [num];
+        int [] counts = new int [num];
+        int index =0;
+        for(   index = 0; index < randomnumbers.length; index++){
+            randomnumbers[index] = ((double)((Math.random() * 50) +1));
+            counts[randomnumbers2[index]]++;
+            
+             System.out.println( index + ": " + randomnumbers[index]);
+        }// for loop
+        /*for (int index = 0; index < counts.length; index++)
+        {
+            if (randomnumbers[index] %2 ==0)
+            {
+                int even = counts[randomnumbers2[index]];
+                while (even > 0){
+                    System.out.print("E");
+                    even--;
+                     System.out.println( index + ": " + randomnumbers[index]);
+                }
+            }
+            else{
+                int odd = counts[index];
+                while (odd > 0)
+                {
+                    System.out.print("O");
+                    odd--;
+                    
+                }
+            }
+            System.out.println();*/
+            
+            for ( int i = counts.length; i > 0; i--)
+            {
+                System.out.println(i + ": " );
+
+            }
+        //end for loop
+        //System.out.println( counts[randomnumbers[index]]);
+    }//end main
+}//end class
