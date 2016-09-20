5f41a30cd178a5738d86b805233083eb2673f093
cmis <cmis@cmis-hs02.(none)>
2016-09-20 11:54:57 +0700

    ahahh

---
+++ OOP/Simpleperson.java
@@ -0,0 +1,8 @@
+ public class Simpleperson{
+    private String name;
+    public static void main(String [] arg)
+    {
+        Simpleperson p1 = new Simpleperson();
+        System.out.println(p1);
+    }
+}
