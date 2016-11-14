4808f7c070477f9e1e717112548d84d5156e7e32
punch2475-cmis <punch2475@cmis.ac.th>
2016-11-14 09:54:26 +0700

    Update Array2D5Songlist.java

--- 2DArray/Array2D5Songlist.java
+++ 2DArray/Array2D5Songlist.java
@@ -1,4 +1,17 @@
 public class Array2D5Songlist
 {
+    public String name;
+    public int rating;
+    
+    public Array2D5Songlist (String name, int rating){
+        this.name = name;
+        this.rating = rating;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getRating(){
+        return rating;
+    }
     
 }
