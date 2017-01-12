8d0345300ad42225d2adced3819b84e5a526b19f
cmis <cmis@cmis-hs00.(none)>
2017-01-12 08:40:41 +0700

    P1

---
+++ Interface/MyCar.java
@@ -0,0 +1,18 @@
+public class MyCar implements Connectable , Discountable
+{
+    private int kpm;
+
+    public MyCar( int KMperMeters){
+        this.kpm = KMperMeters;
+    }
+
+    public void connectToBluetooth()
+    {
+        System.out.println("Connecting to bluetooth for MyCar");
+    }
+
+    public void applyDiscount()
+    {
+        System.out.println( "Discount for MyCar is 10%" );    
+    }
+}
