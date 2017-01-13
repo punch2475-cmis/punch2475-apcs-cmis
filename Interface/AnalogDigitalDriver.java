ddf3f59ceaca88a6ab3e1feae3d8898fc6eab090
cmis <cmis@cmis-hs00.(none)>
2017-01-13 14:40:10 +0700

    nearly done

--- Interface/AnalogDigitalDriver.java
+++ Interface/AnalogDigitalDriver.java
@@ -1,6 +1,18 @@
+import java.util.ArrayList;
 public class AnalogDigitalDriver
 {
-    public void main (String [] args){
+    public static void main (String [] args){
+        BWTV bwtv = new BWTV();
+        HDTV hdtv = new HDTV();
+        ArrayList<TV> tv = new ArrayList<TV>();
+        tv.add(bwtv);
+        tv.add(hdtv);
+        for(TV e : tv){
+            System.out.println(e.tvType() + " ...Am I on? " + e.getTVOn());
+        }
+        for(TV x : tv){
+            System.out.println(x.tvType() + " ...Am I on? " + x.setTVOn(true));
+        }
         
     }
 }
