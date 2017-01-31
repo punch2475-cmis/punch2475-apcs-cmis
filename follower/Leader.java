6cbddc2b6e0cb13649fd79e026385bc093667921
cmis <cmis@cmis-hs00.(none)>
2017-01-31 08:40:18 +0700

    ahhha

--- follower/Leader.java
+++ follower/Leader.java
@@ -13,33 +13,52 @@ public class Leader extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     Actor Target;
+    boolean leader;
     public Leader(){
-
+        leader = true;
     }
 
     public Leader (Actor Target){
         this.Target = Target;
+        leader = false;
     }
 
     public void act() 
     {
         // Add your action code here.
-        move(5);
-        if(isAtEdge() == true){
-            turn(1);
+        //move(5);
+        
+        if(leader == true ){
+            //move(1);
+            //turn(-45);
+            move(2);
+            if (isAtEdge() == true){
+                if (getX() < 10){
+                    setLocation(getX(),getY() -1 );
+                }
+                else if(getY() < 10){
+                    move(2);
+                }
+                else if (getX() > 590){
+                setLocation(getX(),getY()+1);
+            }
+            else {
+                move(-2);
+            }
+            }
         }
-        if(Greenfoot.getRandomNumber(4) == 0){
-            move(1);
-            turn(2);
-
+        else if (leader == false){
+            follow();
         }
+        
+        
     }
 
     public void follow(){
         int targetX = Target.getX();
         int targetY = Target.getY();
 
-        turnTowards((targetX - 10) , (targetY -10));
+        turnTowards((targetX-10 ) , (targetY ));
         if(targetX  == getX() && targetY ==getY()){
             move(0);
         }
@@ -48,3 +67,16 @@ public class Leader extends Actor
         }
     }
 }
+/*else if(leader == true&&Greenfoot.getRandomNumber(4) == 1){
+            move(-1);
+        }
+        else if(leader == true && Greenfoot.getRandomNumber(4) == 2){
+            setLocation(getX(),getY() -1 );
+        }
+        else if(leader == true && isAtEdge() == true){
+            setLocation(getX(),getY()-100);
+            move(-1);
+        }
+        /*if(Greenfoot.getRandomNumber(4) == 3){
+        //turn(25);
+        }*/
--- follower/MyWorld.java
+++ follower/MyWorld.java
@@ -25,20 +25,21 @@ public class MyWorld extends World
         Leader leader = new Leader();
         addObject(leader,224,201);
         Leader leader2 = new Leader(leader);
-        addObject(leader2,231,241);
+        addObject(leader2,leader.getX(),leader.getY()-20);
         Leader leader3 = new Leader(leader2);
-        addObject(leader3,241,241);
+        addObject(leader3,leader.getX(),leader2.getY()-20);
         Leader leader4 = new Leader(leader3);
-        addObject(leader4,251,261);
+        addObject(leader4,leader.getX(),leader3.getY()-20);
         Leader leader5 = new Leader(leader4);
-        addObject(leader5,251,271);
+        addObject(leader5,leader.getX(),leader4.getY()-20);
         Leader leader6 = new Leader(leader5);
-        addObject(leader6,261,291);
-        Leader leader7 = new Leader(leader6);
+        addObject(leader6,leader.getX(),leader5.getY()-20);
+        /*Leader leader7 = new Leader(leader6);
         addObject(leader7,271,301);
         Leader leader8 = new Leader(leader7);
-        addObject(leader7,271,301);
+        /*addObject(leader7,271,301);
         addObject(leader7,271,301);addObject(leader7,271,301);addObject(leader7,271,301);addObject(leader7,271,301);
-        addObject(leader7,271,301);addObject(leader7,271,301);
+        addObject(leader7,271,301);addObject(leader7,271,301);*/
+      
     }
 }
